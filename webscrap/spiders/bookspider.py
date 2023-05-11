796bcd220fde4cdb99404aaabdcf9ebad5968314
nishchaybhuker20 <nishchaybhuker123@gmail.com>
2023-05-11 20:03:24 +0530

    making pipelines

--- webscrap/spiders/bookspider.py
+++ webscrap/spiders/bookspider.py
@@ -31,17 +31,18 @@ class BookspiderSpider(scrapy.Spider):
 
     def parse_book_page(self, response):
         table_rows = response.css("table tr"),
-        book_item = BookItem(),
+        book_item = book_item()
 
-            book_item['url' ]: response.url,
-            book_item['title'] : response.css('.product_main h1::text').get(),
-            book_item['product_type']  : table_rows[1].css('td::text').get(),
-            book_item['price_excl_tax']  : table_rows[2].css('td::text').get(),
-            book_item['price_incl_tax']  : table_rows[3].css('td::text').get(),
-            book_item['tax']  : table_rows[4].css('td::text').get(),
-            book_item['availability']  : table_rows[5].css('td::text').get(),
-            book_item['num_reviews']  : table_rows[6].css('td::text').get(),
-            book_item['stars'] : response.css('p.star-rating').attrib['class'],
+        book_item['url' ]: response.url,
+        book_item['title'] : response.css('.product_main h1::text').get(),
+        book_item['product_type']  : table_rows[1].css('td::text').get(),
+        book_item['price_excl_tax']  : table_rows[2].css('td::text').get(),
+        book_item['price_incl_tax']  : table_rows[3].css('td::text').get(),
+        book_item['tax']  : table_rows[4].css('td::text').get(),
+        book_item['availability']  : table_rows[5].css('td::text').get(),
+        book_item['num_reviews']  : table_rows[6].css('td::text').get(),
+        book_item['stars'] : response.css('p.star-rating').attrib['class'],
 
-
-        }
+        yield book_item
+        
+        
